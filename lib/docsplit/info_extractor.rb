c33b5c135177f34f0e1c73859adb52d6b7897dcd
Doug Mayer <dougtmayer@gmail.com>
2012-02-29 11:48:17 -0600

    That didn't work. Just ignore UTF8 characters extracted instead.

--- lib/docsplit/info_extractor.rb
+++ lib/docsplit/info_extractor.rb
@@ -1,3 +1,5 @@
+require 'iconv'
+
 module Docsplit
 
   # Delegates to **pdfinfo** in order to extract information about a PDF file.
@@ -5,21 +7,22 @@ module Docsplit
 
     # Regex matchers for different bits of information.
     MATCHERS = {
-      :author   => Regexp.new("^Author:\s+([^\n]+)".encode('UTF-8')),
-      :date     => Regexp.new("^CreationDate:\s+([^\n]+)".encode('UTF-8')),
-      :creator  => Regexp.new("^Creator:\s+([^\n]+)".encode('UTF-8')),
-      :keywords => Regexp.new("^Keywords:\s+([^\n]+)".encode('UTF-8')),
-      :producer => Regexp.new("^Producer:\s+([^\n]+)".encode('UTF-8')),
-      :subject  => Regexp.new("^Subject:\s+([^\n]+)".encode('UTF-8')),
-      :title    => Regexp.new("^Title:\s+([^\n]+)".encode('UTF-8')),
-      :length   => Regexp.new("^Pages:\s+([^\n]+)".encode('UTF-8')),
+      :author   => /^Author:\s+([^\n]+)/,
+      :date     => /^CreationDate:\s+([^\n]+)/,
+      :creator  => /^Creator:\s+([^\n]+)/,
+      :keywords => /^Keywords:\s+([^\n]+)/,
+      :producer => /^Producer:\s+([^\n]+)/,
+      :subject  => /^Subject:\s+([^\n]+)/,
+      :title    => /^Title:\s+([^\n]+)/,
+      :length   => /^Pages:\s+([^\n]+)/,
     }
 
     # Pull out a single datum from a pdf.
     def extract(key, pdfs, opts)
       pdf = [pdfs].flatten.first
       cmd = "pdfinfo #{ESCAPE[pdf]} 2>&1"
-      result = `#{cmd}`.chomp
+      # Remove non-ASCII characters as the matcher chokes on them
+      result = Iconv.conv('ASCII//IGNORE', 'UTF8', `#{cmd}`.chomp)
       raise ExtractionFailed, result if $? != 0
       match = result.match(MATCHERS[key])
       answer = match && match[1]
